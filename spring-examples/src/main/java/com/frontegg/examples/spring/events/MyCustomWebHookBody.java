343d2a8e15aca427d1bf4d102c7fd0c92164e3af
ARTHUR <artur.hovhannisyan@wcfsmart.com>
2020-12-09 18:38:12 +0400

    events example

---
+++ spring-examples/src/main/java/com/frontegg/examples/spring/events/MyCustomWebHookBody.java
@@ -0,0 +1,15 @@
+package com.frontegg.examples.spring.events;
+
+import com.frontegg.sdk.events.channels.WebhookBody;
+
+public class MyCustomWebHookBody implements WebhookBody {
+    private String field;
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+}
--- spring-examples/src/main/java/com/frontegg/examples/spring/events/TriggerEventController.java
+++ spring-examples/src/main/java/com/frontegg/examples/spring/events/TriggerEventController.java
@@ -9,12 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/events")
+@RestController
+@RequestMapping(value = "/events")
 public class TriggerEventController {
 
     @Autowired
@@ -22,29 +20,34 @@ public class TriggerEventController {
 
     @RequestMapping(value = "/trigger",
             method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+            consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<EventResponse> triggerEvents(@RequestParam String eventKey,
                                                        @RequestParam String tenantId,
                                                        @RequestParam String title,
                                                        @RequestParam String description) {
 
         EventProperties eventProperties = new DefaultEventProperties(title, description);
-        TriggerOptions<EventProperties> options = new TriggerOptions<>();
-        ChannelsConfiguration channelsConfiguration = new ChannelsConfigurationBuilder()
-                .defaultWebhook().build();
+        TriggerOptions<EventProperties> options = new TriggerOptions();
+        MyCustomWebHookBody webHookBody = new MyCustomWebHookBody();
+        webHookBody.setField("asdas");
+
+        ChannelsConfiguration channelsConfiguration = ChannelsConfigurationBuilder.getInstance()
+                .webhook(webHookBody)
+                .build();
+
         options.setChannels(channelsConfiguration);
         options.setEventKey(eventKey);
         options.setTenantId(tenantId);
         options.setProperties(eventProperties);
         EventResponse response = eventsClient.trigger(options);
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
+        return new ResponseEntity(response, HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/{eventId}",
             method = RequestMethod.GET,
-            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+            consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<EventStatuses> getEventStatus(@PathVariable String eventId) {
         EventStatuses statuses = eventsClient.getEventStatus(eventId);
-        return new ResponseEntity<>(statuses, HttpStatus.OK);
+        return new ResponseEntity(statuses, HttpStatus.OK);
     }
 }
